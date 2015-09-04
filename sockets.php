1cc047a882a8ba090a5f7224ca5cb66ac2f1ec01
Derek Bourgeois <derek@ibourgeois.com>
2015-09-04 16:14:21 -0400

    Update sockets.php

--- sockets.php
+++ sockets.php
@@ -1,13 +1,33 @@
 <?php
 
-  // Create an SMTP Socket
-  $socket  = socket_create(AF_INET, SOCK_STREAM, 0);
-  $host    = '127.0.0.1';
-  $port    = '25';
-  $bind    = socket_bind($socket, [$host]);
-  $smtp    = socket_connect($socket, $address, $port);
+    // Configure Socket
+    $host    = '127.0.0.1';
+    $port    = '25';
+
+    // Create a TCP over IPv4 Socket
+    $socket  = socket_create(AF_INET, SOCK_STREAM, 0);
   
-  // Start Listening on port 25
-  $listen = socket_listen($smtp); 
-  
-  
+    if(!$socket)
+    {
+        $errorcode = socket_last_error();
+        $errormsg  = socket_strerror($errorcode);
+     
+        die("Couldn't create socket: [$errorcode] $errormsg \n");
+    }
+    
+    // Connect to socket
+    $smtp = socket_connect($socket, $address, $port);
+    
+    if(!$smtp)
+    {
+        $errorcode = socket_last_error();
+        $errormsg  = socket_strerror($errorcode);
+     
+        die("Could not connect: [$errorcode] $errormsg \n");
+    }
+    
+    
+    
+    
+    // Close the socket
+    socket_close($socket);
